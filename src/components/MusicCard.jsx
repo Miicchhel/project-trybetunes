4ad6e5a75b5b1d3cbfab520674e3042b827c64cf
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-01 18:52:49 -0300

    Criando a lista de músicas do álbum selecionado

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class MusicCard extends React.Component {
+  render() {
+    const { previewUrl, trackName } = this.props;
+    return (
+      <div>
+        <span>{ trackName }</span>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params : { id } } } = this.props;
+    this.fetchGetMusics(id);
+  }
+
+  fetchGetMusics = async (id) => {
+    const response = await getMusics(id);
+    this.setState({
+      artistName: response[0].artistName,
+      albumName: response[0].collectionName,
+      data: response.filter((item, index) => (index !== 0)),
+    });
+  };
+
   render() {
+    const { data, artistName, albumName } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <p>Album</p>
+        <h2 data-testid="album-name">{ albumName }</h2>
+        <h3 data-testid="artist-name">{ artistName }</h3>
+        {
+          data.map((item) => (
+            <MusicCard
+              key={ item.trackId }
+              previewUrl={ item.previewUrl }
+              trackName={ item.trackName }
+            />
+          ))
+        }
       </div>
     );
   }
